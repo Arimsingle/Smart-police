dae2d1dbea529c72b975eb00bbeb36cfddcf5741
Arimsingle <arim.coe60@gmail.com>
2020-08-31 23:50:04 +0700

    fixing send&sign transaction function

--- back-end/src/services/sendSign.js
+++ back-end/src/services/sendSign.js
@@ -5,15 +5,15 @@ const sendSignTransaction = async (templeteObj) => {
         // templeteObj.templete
         const dataEncode = await templeteObj.templete.encodeABI();
         const gas = await templete.estimateGas({ from: templeteObj.from });
-        const ethBalance = await web3.eth.getBalance(_Account.address);
+        const ethBalance = await web3.eth.getBalance(templeteObj.from);
         if (ethBalance < gas) {
             return res.json({
                 message: "Not enough eth coin"
             });
         }
-        const nonce = await web3.eth.getTransactionCount(_Account.address);
-        const rawTx = tranferData(nonce, gas, dataEncode, dotenv.parsed.CONTRACT_ADDRESS);
-        const decryptedPrivateKey = crypto.decrypt(_EncryptedPrivateKey, req.body._Password).slice(2);
+        const nonce = await web3.eth.getTransactionCount(templeteObj.from);
+        const rawTx = tranferData(nonce, gas, dataEncode, templeteObj.contract);
+        const decryptedPrivateKey = crypto.decrypt(templeteObj.PrivateKey, req.body._Password).slice(2);
         const privateKey = Buffer.from(decryptedPrivateKey, 'hex');
         const tx = new Tx(rawTx);
         tx.sign(privateKey);
